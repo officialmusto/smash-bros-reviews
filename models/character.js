56642b117ddc1a8aead6a6cfbd5a1f3a013477ee
Christian Musto <christianmustoj@gmail.com>
2023-04-24 13:41:10 -0400

    add drop-down list

--- models/character.js
+++ models/character.js
@@ -2,9 +2,15 @@ import mongoose from 'mongoose'
 
 const Schema = mongoose.Schema
 
+const reviewSchema = new Schema({
+  content: String,
+  rating: {type: Number, min: 1, max: 5, default: 5}
+}, {
+  timestamps: true
+})
 const characterSchema = new Schema({
   name: String,
-  content: String,
+  reviews: [reviewSchema],
   avatar: String,
 }, {
   timestamps: true,
--- models/profile.js
+++ models/profile.js
@@ -5,6 +5,8 @@ const Schema = mongoose.Schema
 const profileSchema = new Schema({
   name: String,
   avatar: String,
+  isAdmin: Boolean,
+  favChar: { type: Schema.Types.ObjectId, ref: 'Character'}
 }, {
   timestamps: true
 })
